b52ff0287e9b128540aeba0aa1878a91f5877030
Evan Medrano <evan.a.medrano@gmail.com>
2020-06-07 16:21:58 -0400

    Add tests for rawg_api/request file

--- lib/rawg_api/request.rb
+++ lib/rawg_api/request.rb
@@ -4,10 +4,10 @@ module RawgApi
   class Request
     include HTTParty
     base_uri "api.rawg.io/api"
-    USER_AGENT = { "User-Agent" => "GoodGames (+gooooodgames.herokuapp.com)" }
+    headers "User-Agent" => "GoodGames (+gooooodgames.herokuapp.com)"
 
     def get(path, options = {})
-      super(path, { headers: USER_AGENT, debug_output: STDOUT }.merge(options))
+      super(path, options)
     end
   end
 end
