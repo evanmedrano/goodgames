e228341ea835e830a00376b2944af1a4178c940b
Evan Medrano <evan.a.medrano@gmail.com>
2020-06-11 23:02:38 -0400

    Refactor comments index and add specs for invalid comment routes

--- spec/features/user_updates_a_game_comment_spec.rb
+++ spec/features/user_updates_a_game_comment_spec.rb
@@ -15,13 +15,40 @@ feature "user updates a game comment" do
     end
   end
 
-  scenario "as another user" do
+  scenario "as another user visiting games show page" do
     VCR.use_cassette("user updates a game comment as another user") do
       user, game = create(:user), create(:game)
       create(:comment, commentable: game, user: user)
       visit game_path(game, as: logged_in_user)
 
       expect(page).not_to have_link("Edit comment")
+    end
+  end
+
+  scenario "as another user visiting edit link directly" do
+    VCR.use_cassette("another user visits edit game comment path directly") do
+      user, game = create(:user), create(:game)
+      comment = create(:comment, commentable: game, user: user)
+      visit edit_game_comment_path(game, comment, as: logged_in_user)
+
+      expect(page).to have_content("You do not have access to this page.")
+    end
+  end
+
+  scenario "that does not exist" do
+    VCR.use_cassette("user views game comment that does not exist") do
+      game = create(:game)
+      visit edit_game_comment_path(game, 1)
+
+      expect(page).to have_content("Game over!")
+    end
+  end
+
+  scenario "for a game that does not exist" do
+    VCR.use_cassette("user views game comment of a non-existing game") do
+      visit edit_game_comment_path(1, 1)
+
+      expect(page).to have_content("Game over!")
     end
   end
 
